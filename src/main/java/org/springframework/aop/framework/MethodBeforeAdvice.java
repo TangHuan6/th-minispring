1dda1d6908d941aeb89d629729a172a7a52b3213
hj <3130811346@qq.com>
2025-07-02 17:09:51 +0800

    BeforeAdvice

---
+++ src/main/java/org/springframework/aop/framework/MethodBeforeAdvice.java
@@ -0,0 +1,10 @@
+package org.springframework.aop.framework;
+
+import org.springframework.aop.BeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public interface MethodBeforeAdvice extends BeforeAdvice {
+
+    void before(Method method, Object[] args, Object target) throws Throwable;
+}

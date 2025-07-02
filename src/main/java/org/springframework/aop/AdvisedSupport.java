fadff80ab34413142c573a2008c03f7a19b673c2
hj <3130811346@qq.com>
2025-07-02 16:59:55 +0800

    AOP代理工厂

--- src/main/java/org/springframework/aop/AdvisedSupport.java
+++ src/main/java/org/springframework/aop/AdvisedSupport.java
@@ -11,12 +11,23 @@ import java.lang.reflect.Method;
 
 public class AdvisedSupport {
 
+    //是否使用cglib代理
+    private boolean proxyTargetClass = false;
+
     // 被代理的目标对象
     private TargetSource targetSource;
     // 方法拦截器
     private MethodInterceptor methodInterceptor;
     // 方法匹配器(检查目标方法是否符合通知条件)
     private MethodMatcher methodMatcher;
+
+    public boolean isProxyTargetClass() {
+        return proxyTargetClass;
+    }
+
+    public void setProxyTargetClass(boolean proxyTargetClass) {
+        this.proxyTargetClass = proxyTargetClass;
+    }
 
     public TargetSource getTargetSource() {
         return targetSource;
---
+++ src/main/java/org/springframework/aop/framework/ProxyFactory.java
@@ -0,0 +1,29 @@
+package org.springframework.aop.framework;
+
+import org.springframework.aop.AdvisedSupport;
+
+/**
+ * @author tanghuan
+ * @date 2025/7/2
+ */
+
+public class ProxyFactory {
+    private AdvisedSupport advisedSupport;
+
+    public ProxyFactory(AdvisedSupport advisedSupport) {
+        this.advisedSupport = advisedSupport;
+    }
+
+    public Object getProxy() {
+        return createAopProxy().getProxy();
+    }
+
+    private AopProxy createAopProxy() {
+        if (advisedSupport.isProxyTargetClass()) {
+            return new CglibAopProxy(advisedSupport);
+        }
+
+        return new JdkDynamicAopProxy(advisedSupport);
+    }
+
+}

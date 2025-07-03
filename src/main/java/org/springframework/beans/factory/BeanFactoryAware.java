951c22b72384b57f3bd2dfcf02aa3d84e5a1797b
hj <3130811346@qq.com>
2025-07-03 15:46:00 +0800

    动态代理融入bean生命周期

--- src/main/java/org/springframework/beans/factory/BeanFactoryAware.java
+++ src/main/java/org/springframework/beans/factory/BeanFactoryAware.java
@@ -5,7 +5,7 @@ import org.springframework.beans.BeansException;
 /**
  * 实现该接口，能感知所属BeanFactory
  */
-public interface BeanFactoryAware {
+public interface BeanFactoryAware extends Aware{
 
     void setBeanFactory(BeanFactory beanFactory) throws BeansException;
 }
